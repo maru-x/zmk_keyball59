15539bfc5c77ace375ad5c802c91dead4f199fde
maru-x <m_goshono@yahoo.co.jp>
2025-06-02 18:28:46 +0900

    設定ファイルの修正: input_pmw3360.cでzmk/layer.hをzmk/layers.hに修正

--- drivers/input/input_pmw3360.c
+++ drivers/input/input_pmw3360.c
@@ -12,7 +12,7 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/util.h>
 #include "input_pmw3360.h"
-#include <zmk/layer.h>
+#include <zmk/layers.h>
 #include <zephyr/sys/atomic.h>
 
 LOG_MODULE_REGISTER(pmw3360, CONFIG_INPUT_LOG_LEVEL);
